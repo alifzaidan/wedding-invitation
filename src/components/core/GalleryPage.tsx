fb9aa6b6e5ed8a965308ead44e6288c0be21487f
alifzaidan <zaidanalif22710@gmail.com>
2024-09-07 14:17:47 +0700

    update feature in gallery page

--- src/components/core/GalleryPage.tsx
+++ src/components/core/GalleryPage.tsx
@@ -1,28 +1,94 @@
 import { Vidaloka } from 'next/font/google';
+import Image from 'next/image';
+import { useState } from 'react';
+import Slider from 'react-slick';
+import Lightbox from 'yet-another-react-lightbox';
+import 'yet-another-react-lightbox/styles.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const vidaloka = Vidaloka({
     subsets: ['latin'],
     weight: '400',
 });
 
+const images = [
+    '/assets/img/gallery-1.jpg',
+    '/assets/img/gallery-2.jpg',
+    '/assets/img/gallery-3.jpg',
+    '/assets/img/gallery-4.jpg',
+    '/assets/img/gallery-5.png',
+    '/assets/img/gallery-6.jpg',
+    '/assets/img/gallery-7.jpeg',
+    '/assets/img/gallery-8.jpg',
+    '/assets/img/gallery-1.jpg',
+];
+
 export default function GalleryPage() {
+    const [isOpen, setIsOpen] = useState(false);
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const openLightbox = (index: any) => {
+        setCurrentIndex(index);
+        setIsOpen(true);
+    };
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    };
+
     return (
         <div id="gallery" className="sm:py-16 py-8 sm:px-8 px-4 bg-gradient-to-br from-stone-700 to-stone-300">
             <div className="text-center text-white">
                 <h1 className={`${vidaloka.className} sm:text-5xl text-3xl mb-4`}>Captured Moment</h1>
                 <p className="sm:text-lg text-sm">Every Love Story Is Beautiful, But Ours Is Favorite.</p>
-                <div className="grid grid-cols-3 gap-6 mt-8">
-                    <div className="bg-stone-200 h-24 rounded-xl"></div>
-                    <div className="bg-stone-200 h-24 rounded-xl"></div>
-                    <div className="bg-stone-200 h-24 rounded-xl"></div>
-                    <div className="bg-stone-200 h-24 rounded-xl"></div>
-                    <div className="bg-stone-200 h-24 rounded-xl"></div>
-                    <div className="bg-stone-200 h-24 rounded-xl"></div>
-                    <div className="bg-stone-200 h-24 rounded-xl"></div>
-                    <div className="bg-stone-200 h-24 rounded-xl"></div>
-                    <div className="bg-stone-200 h-24 rounded-xl"></div>
+
+                <div className="flex flex-col gap-2 items-center mt-8">
+                    <Slider {...settings} className="mb-6 w-full sm:w-2/3">
+                        {images.slice(0, 8).map((src, index) => (
+                            <div
+                                key={index}
+                                className="bg-stone-200 dm:rounded-xl rounded-lg overflow-hidden cursor-pointer"
+                                onClick={() => openLightbox(index)}
+                            >
+                                <Image
+                                    width={1000}
+                                    height={1000}
+                                    src={src}
+                                    alt={`Carousel Image ${index + 1}`}
+                                    className="w-full h-auto object-cover aspect-video"
+                                />
+                            </div>
+                        ))}
+                    </Slider>
+
+                    <div className="grid grid-cols-2 sm:grid-cols-4 gap-4 w-full sm:w-2/3">
+                        {images.slice(0, 8).map((src, index) => (
+                            <div
+                                key={index + 3}
+                                className="bg-stone-200 md:rounded-xl rounded-lg overflow-hidden cursor-pointer"
+                                onClick={() => openLightbox(index + 3)}
+                            >
+                                <Image
+                                    width={1000}
+                                    height={1000}
+                                    src={src}
+                                    alt={`Gallery Image ${index + 4}`}
+                                    className="w-full h-full object-cover"
+                                />
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
+
+            {isOpen && <Lightbox slides={images.map((src) => ({ src }))} open={isOpen} close={() => setIsOpen(false)} index={currentIndex} />}
         </div>
     );
 }
